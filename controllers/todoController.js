8ac6db3b1242acd1ed543d846d143de4f8d7ddf1
emwno <pulse.aashir@gmail.com>
2018-07-08 17:08:44 +0500

    added todo syncing support with backendless

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,18 +1,35 @@
-let data = [];
+let localTodoData = [];
+const Todo = require('../model/todo.js');
 
-module.exports = function(app) {
+module.exports = function(app, backendless) {
 
     app.get('/', function(req, res) {
-        res.render('index', {
-            todos: data
-        });
+
+        Backendless.Data.of(Todo).find()
+            .then(function(result) {
+                localTodoData = result;
+                res.render('index', {
+                    todos: result
+                });
+            })
+            .catch(function(error) {
+                console.log('Error: ' + error);
+            });
     });
 
     app.post('/', function(req, res) {
-        data.push(req.body.todoItem);
-        res.render('index', {
-            todos: data
-        });
+        let todoClass = new Todo(req.body.todoItem);
+
+        backendless.Data.of(Todo).save(todoClass)
+            .then(function(obj) {
+                localTodoData.push(obj);
+                res.render('index', {
+                    todos: localTodoData
+                });
+            })
+            .catch(function(error) {
+                console.log('Error: ' + error);
+            });
     });
 
     app.delete('/', function(req, res) {
---
+++ model/todo.js
@@ -0,0 +1,10 @@
+class Todo {
+
+    constructor(data, created) {
+        this.data = data;
+        this.created = created;
+    }
+
+}
+
+module.exports = Todo;
